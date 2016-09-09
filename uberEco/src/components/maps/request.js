1d67b3cf11ef5a40f13b4f1474bd168437a7834e
shea12 <cshearun12@gmail.com>
2016-09-09 16:35:05 -0700

    add example points on map

--- uberEco/src/components/maps/request.js
+++ uberEco/src/components/maps/request.js
@@ -4,33 +4,17 @@ import {
   Text,
   View,
   TextInput,
-  ScrollView
+  MapView
 } from 'react-native';
-
-import MapView from 'react-native-maps';
-import Geocoder from 'react-native-geocoder';
 
 var Button = require('../common/button');
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      region: {
-        latitude: this.props.route.location.lat,
-        longitude: this.props.route.location.lng,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005
-      },
-      annotation: {
-        latitude: this.props.route.location.lat,
-        longitude: this.props.route.location.lng,
-      },
-      destination: {
-        formattedAddress: '',
-        position: {
-          lat: 0,
-          lng: 0
-        }
+      location: {
+        latitude: 0,
+        longitude: 0
       }
     };
   },
@@ -40,33 +24,18 @@ module.exports = React.createClass({
         
         <View style={styles.top}>
           <Text style={styles.subheader}>Enter Destination</Text>
-          <TextInput 
-            style={styles.input} 
-            onChange={this.onInputChange}
-          />
-          <Text style={styles.address}>{this.state.destination.formattedAddress}</Text>
-
+          <TextInput style={styles.input} />
         </View>
 
-        <MapView 
+        <MapView
           style={styles.map}
-          initialRegion={this.state.region}
-          showsUserLocation={true}
-          onRegionChange={this.onRegionChange}
           onRegionChangeComplete={this.onRegionChangeComplete}
-          showsPointsOfInterest={false}
-        >
-          <MapView.Marker 
-            coordinate={this.state.annotation}
-            ref={ref => { this.annotation = ref; }}
-          >
-            <MapView.Callout style={styles.callout}>
-              <View>
-                <Text>set location</Text>
-              </View>
-            </MapView.Callout>
-          </MapView.Marker>
-        </MapView>
+          annotations={[this.state.location]}
+          showsUserLocation={true}
+          followUserLocation={true}
+          scrollEnabled={false}
+          zoomEnabled={false}
+        />
 
         <View style={styles.bottom}>
           <Button text={'request ride'} onPress={this.onRequestPress} />
@@ -75,34 +44,15 @@ module.exports = React.createClass({
       </View>
     );
   },
-  onInputChange: function(event) {
-    Geocoder.geocodeAddress(event.nativeEvent.text)
-      .then(res => {
-        this.setState({destination: res[0]});
-      })
-      .catch(err => console.log(err));
-  },
-  onRegionChange: function(region) {
+  onRegionChangeComplete: function(region) {
     this.setState({
-      annotation: {
+      location: {
         latitude: region.latitude,
         longitude: region.longitude
       }
     });
   },
-  onRegionChangeComplete: function(region) {
-    this.annotation.showCallout();
-  },
   onRequestPress: function() {
-    var origin = {
-      lat: this.state.annotation.latitude,
-      lng: this.state.annotation.longitude
-    }
-    var destination = {
-      lat: this.state.destination.position.lat,
-      lng: this.state.destination.position.lng
-    }
-    console.log(origin, destination);
     this.props.navigator.push({name: 'navigation'});
   }
 });
@@ -112,10 +62,10 @@ var styles = StyleSheet.create({
     flex: 1,
   },
   top: {
-    flex: 3
+    flex: 2
   },
   map: {
-    flex: 9
+    flex: 11
   },
   bottom: {
     flex: 1,
@@ -126,6 +76,7 @@ var styles = StyleSheet.create({
     height: 40,
     borderColor: '#777',
     borderWidth: 1,
+    borderRadius: 5,
     margin: 5,
     width: 300,
     alignSelf: 'center'
@@ -135,15 +86,5 @@ var styles = StyleSheet.create({
     color: '#555',
     alignSelf: 'center',
     marginTop: 20
-  },
-  address: {
-    fontSize: 18,
-    color: '#428bca',
-    alignSelf: 'center',
-    width: 300,
-    marginTop: 5
-  },
-  callout: {
-    width: 75
   }
 });
